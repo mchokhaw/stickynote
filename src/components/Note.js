a527ca0adbf1b542aef61908ad90b69fe231a235
Mihir chokhawala <mihirchokhawala@Mickys-macbook.local>
2023-04-07 18:07:02 -0400

    initial commit

---
+++ src/components/Note.js
@@ -0,0 +1,47 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import "../styles/Note.css";
+
+import { FaTrashAlt } from "react-icons/fa";
+
+const Note = (props) => {
+
+    function onClick() {
+        props.onClick();
+    }
+
+    function onMouseEnter() {
+        props.onMouseEnter();
+    }
+
+    function onMouseLeave() {
+        props.onMouseLeave();
+    }
+
+    return (
+        <div
+            className={"Note"}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+        >
+            <h4 className={"NoteText"}>{props.note}</h4>
+            <div className={"NoteClose"} onClick={onClick}>
+                <FaTrashAlt />
+            </div>
+        </div>
+    );
+
+};
+
+Note.defaultProps = {
+    note: ""
+};
+
+Note.propTypes = {
+    note: PropTypes.string,
+    onClick: PropTypes.func,
+    onMouseEnter: PropTypes.func,
+    onMouseLeave: PropTypes.func
+};
+
+export default Note;
